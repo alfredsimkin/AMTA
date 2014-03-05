6bbd4f314208677c7c91e0c3771b35e683f8076b
alfredsimkin <alfred.simkin@gmail.com>
2014-03-05 16:25:45 -0500

    test

--- remove_multi_exon_UTRs.py
+++
@@ -1,5 +0,0 @@
-for line in open('human_refseq_converted_coords'):
-	line=line.strip()
-	UTR_3list=line.split('\t')[8].split(',')
-	if len(UTR_3list)==1:
-		print line
--- remove_non_seq.py
+++
@@ -1,3 +0,0 @@
-import sys
-data_folder=sys.argv[1]
-maf_input=sys.argv[2]
