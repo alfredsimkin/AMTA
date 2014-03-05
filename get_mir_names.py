db914f7c5557e1c08d0e4f65b935e216e9605158
alfredsimkin <alfred.simkin@gmail.com>
2014-03-05 16:19:10 -0500

    test

--- get_mir_names.py
+++
@@ -1,9 +0,0 @@
-final_list=[]
-value_list=[line.strip().split() for line in open('values')]
-mirlist=[line.strip().split() for line in open('enrich_depletion_results_lumped_vert_seeds')]
-for value_line in value_list:
-	for mirline in mirlist:
-		if mirline[1].replace('U', 'T')==value_line[0] and value_line[0] not in final_list:
-			print value_line[0], mirline[4:]
-			final_list.append(value_line[0])
-			
--- junk2.py
+++
@@ -1,8 +0,0 @@
-final_probs=[line.strip().split() for line in open('final_probs')]
-real_eightmers=[line.strip().split() for line in open('real_eightmers')]
-
-
-for line in final_probs:
-	for eightmer in real_eightmers:
-		if line[0]==eightmer[-1]:
-			print '\t'.join(line)+'\t'+'\t'.join(eightmer[:-1])
